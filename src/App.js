bb8c510af90188ff2f0b3b2e9ade51fc94ae06f8
NicoLaval <nicolas.laval@making-sense.info>
2023-10-26 08:50:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Grid } from "./grid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>1 - Resize neighbors instead of pushing them</h2>
+			<Grid
+				bLabel="Do not push me below, resize me"
+				resizeHandles={["e", "w"]}
+			/>
+			<h2>2 - Resize neighbors together</h2>
+			<Grid
+				bLabel="Resize my height leaving it equal to that of A"
+				resizeHandles={["s"]}
+			/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/grid/layout.js
@@ -0,0 +1,20 @@
+export const layout = [
+	{
+		i: "A",
+		x: 0,
+		y: 0,
+		h: 10,
+		w: 6,
+		minW: 2,
+		maxW: 10,
+	},
+	{
+		i: "B",
+		x: 6,
+		y: 0,
+		h: 10,
+		w: 6,
+		minW: 2,
+		maxW: 10,
+	},
+];
